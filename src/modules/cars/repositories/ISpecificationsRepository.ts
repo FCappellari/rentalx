06f8951dd6e54ed35b1d553df94884f5b973358f
Fabrício Cappellari <fabricio.cappellari@live.com>
2021-12-15 20:38:23 -0300

    Adding specification model

---
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -0,0 +1,14 @@
+import { Specification } from "../model/Specification";
+
+interface ICreateSpecificationDTO {
+    name: string;
+    description: string;
+}
+
+interface ISPecificationsRepository {
+    create({ name, description }: ICreateSpecificationDTO) : void;
+    findByName(name: string): Specification;
+    list(): Specification[];
+}
+
+export { ISPecificationsRepository, ICreateSpecificationDTO }
---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,25 @@
+import { ISPecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+class CreateSpecificationService {
+    constructor(private speficitationsRepository: ISPecificationsRepository) { }
+
+    execute({ name, description }: IRequest): void {
+        const speficitationAlreadyExists = this.speficitationsRepository.findByName(name);
+
+        if (speficitationAlreadyExists) {
+            throw new Error("Speficitation already exists!");
+        }
+
+        this.speficitationsRepository.create({
+            name,
+            description
+        })
+    }
+}
+
+export { CreateSpecificationService }
