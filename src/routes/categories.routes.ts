43a3bf303da269d1e286a84e16b2fb740690e348
Fabrício Cappellari <fabricio.cappellari@live.com>
2021-12-16 21:34:39 -0300

    Changed directory and start work with useCases

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,28 +1,19 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post("/", (req, res) => {
-    const { name, description } = req.body;
+categoriesRoutes.post("/", (request, response) => {
+    return createCategoryController.handle(request, response);
+})
 
-    const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-    createCategoryService.execute({ 
-        name, 
-        description
-    });
-
-    return res.status(201).send();
-});
-
-categoriesRoutes.get("/", (req, res) => {
+categoriesRoutes.get("/", (request, response) => {
     const all = categoriesRepository.list();
 
-    return res.json(all);
+    return response.json(all);
 })
 
 export { categoriesRoutes };
